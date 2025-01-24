7cfd4e19a798a8d4054b2c688ab82eccb858d0b3
Ashymov Aziret <aziretashym@gmail.com>
2025-01-24 18:57:57 +0600

    [API] Created model for users and made routes for registration, login and logout.

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import usersRouter from "./routes/users";
+import mongoose from "mongoose";
+import mongoDb from "./mongoDb";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/users', usersRouter);
+
+
+const run = async () => {
+
+    await  mongoose.connect('mongodb://localhost:27017/abc');
+
+    app.listen(port, () => {
+        console.log(`Server started on port http://localhost:${port}`);
+    });
+
+    process.on('exit', () => {
+        mongoDb.disconnect();
+    });
+
+};
+
+run().catch((e) => console.error(e));
